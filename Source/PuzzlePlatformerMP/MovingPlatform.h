fcb84d5e770633c49521400d6a06ee8be3e0b64c
kprasad1221 <kprasad.1221@gmail.com>
2020-07-05 19:49:10 -0800

    Moving Platform Code

---
+++ Source/PuzzlePlatformerMP/MovingPlatform.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "MovingPlatform.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMERMP_API AMovingPlatform : public AStaticMeshActor
+{
+	GENERATED_BODY()
+
+public:
+	AMovingPlatform();
+
+protected:
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	float Speed = 7.5f;
+	
+};
